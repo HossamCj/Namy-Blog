4ee96354d449a4c67f4d80497537d1248a063f48
Hossam Cj <hossamcj@gmail.com>
2021-09-15 20:45:38 +0200

    renew the migrations

--- property/views.py
+++ property/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 
 from .models import Property
@@ -14,4 +14,8 @@ class PropertyList(ListView):
 class PropertyDetail(DetailView):
     model = Property
     # book
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['related'] = Property.objects.filter(category=self.get_object().category)[:2]
 
