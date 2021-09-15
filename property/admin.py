cf5bcacf01c2001eff2b03b8c552ace4b0963d93
Hossam Cj <hossamcj@gmail.com>
2021-09-15 20:32:22 +0200

    Adding summernote package

--- property/admin.py
+++ property/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 
 from .models import *
 
 
-admin.site.register(Property)
+# Apply summernote to all TextField in model.
+class SomeModelAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
+    summernote_fields = '__all__'
+
+
+admin.site.register(Property, SomeModelAdmin)
 admin.site.register(Place)
 admin.site.register(Category)
 admin.site.register(PropertyBook)
